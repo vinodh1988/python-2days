733a0bf9850213dafb9f958543832c55ddf807b4
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-02 11:11:36 +0530

    function-return

--- functional/closures.py
+++ functional/closures.py
@@ -1,13 +1,17 @@
-def outer():
+def outer(param):
     print("In outer function")
     a=40
+    state=param
+    #inner function definition
     def inner():
         
         b=80
         nonlocal a
         a=150
         print("inner function")
-        print("still in inner function, a is %d, b is %d"%(a,b))
+        print("still in inner function, a is %d, b is %d and state is %s"%(a,b,state))
         print("exiting inner function")
-    inner()
+    # again in outer
     print("Continuing outer function, a is %d"%(a));
+    return inner
+   
--- main2.py
+++ main2.py
@@ -1,3 +1,9 @@
 from functional import outer
 
-outer()
+reference = outer("Great world!!")
+reference2 = outer("Greater World!!!");
+
+print('------------------------------------------------')
+
+reference();
+reference2();
