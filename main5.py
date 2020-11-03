e158e88146fbc0b4998ec00d6359950d39111109
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-03 08:53:37 +0530

    parameters-commit

---
+++ main5.py
@@ -0,0 +1,18 @@
+#python directly dont support the concept of overloading
+#indirectly we can achieve it by using 
+    # function with default parameters
+    # function with iterable as parameters
+def params(*args,**kwargs):
+    for x in args:
+        print(x)
+  
+    print(kwargs)
+    print("-----------------------------------")
+
+def fun(a,b):
+    print(a,b)
+
+params(1,4,5,pen=34,paper=90)
+params(2,3,4)
+params('Lara','Janak');
+fun(b=53,a="jack")
