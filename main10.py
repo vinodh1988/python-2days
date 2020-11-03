c2ec185b36d02a45119ec71e81891a0757761ece
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-03 11:29:24 +0530

    gen-syntax-sugar

--- main10.py
+++ main10.py
@@ -7,3 +7,12 @@ print(x)
 for a in x:
     print(a)
     print(f'processing {a} going to retreive next')
+
+y = ['ray','ravi','Ajay','ashok','lewis','harry']
+result=[i.upper() for i in y]
+result2=(i.upper() for i in y)
+print(result)
+print(result2)
+
+for a in result2:
+    print(a)
