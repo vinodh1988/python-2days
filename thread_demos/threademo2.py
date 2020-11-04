47ed999a9defc6e7f34297db046445d211d70bc8
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-04 09:47:01 +0530

    lock-commit

---
+++ thread_demos/threademo2.py
@@ -0,0 +1,36 @@
+import threading
+import random
+import time
+
+class SharedResource:
+    def __init__(self,init=0):
+        self.state = init
+        self.lock = threading.Lock()
+    
+    def stateChanger(self):
+        print('Inside thread ',threading.currentThread())
+        print(threading.currentThread(), ' trying to acquire the lock')
+        self.lock.acquire()
+        print(threading.currentThread(), '  acquired the lock')
+        temp = random.randint(0,100)
+        self.state = self.state + temp
+        print(threading.currentThread()," beforeProcessing ",self.state)
+        time.sleep(1)
+        print(threading.currentThread()," processed state ",self.state)
+        self.lock.release()
+        print(threading.currentThread(), 'released the lock')
+
+def call(shared):
+    for a in range(5):
+        shared.stateChanger()
+        
+
+shared = SharedResource(10)
+t1 = threading.Thread(target=call,args=(shared,),name="Star Thread") 
+t2 = threading.Thread(target=call,args=(shared,),name="Sky Thread") 
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
